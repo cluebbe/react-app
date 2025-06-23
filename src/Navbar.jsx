367cc6fea7043cc4316157abb7d6a09f46ea0f0b
Carlos Luebbe <carlosluebbe@Carloss-MacBook-Air.local>
2025-06-23 12:53:22 +0200

    added navbar with home component

---
+++ src/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link} from 'react-router-dom';
+
+import './Navbar.css';
+
+const Navbar = () => {
+
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="nav-links nav-links-left">
+          <Link to="/" className="nav-link">Home</Link>
+         
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
